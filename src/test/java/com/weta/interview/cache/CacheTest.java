2b0e3b51b404ac85209ea8bc9896ff0aabb33e0f
RNS5 <rocksnslopes@yahoo.com>
2021-02-14 22:21:55 -0800

    Unit test

---
+++ src/test/java/com/weta/interview/cache/CacheTest.java
@@ -0,0 +1,13 @@
+package com.weta.interview.cache;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class CacheTest {
+
+    @Test
+    public void TestCache()  {
+        Assertions.assertTrue(true);
+    }
+
+}
